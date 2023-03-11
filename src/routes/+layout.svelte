7884f4f6bf165fbf6281b8fa63cc75b9a5fbbfa2
James Yong <james@konvoykegs.com>
2023-03-11 17:04:12 +1100

    feat: added UIShell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,42 @@
 <script>
-	import { Grid, Row, Column } from 'carbon-components-svelte';
-	import 'carbon-components-svelte/css/white.css';
+  import 'carbon-components-svelte/css/white.css';
+  import {
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    HeaderNavMenu,
+    SkipToContent,
+    Content,
+    Grid,
+    Row,
+    Column,
+  } from "carbon-components-svelte";
+
+  let isSideNavOpen = false;
 </script>
 
-<slot />
+<Header company="Katch Assets" platformName="Live Tracking" bind:isSideNavOpen>
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+  <HeaderNav>
+    <HeaderNavItem href="/" text="Link 1" />
+    <HeaderNavItem href="/" text="Link 2" />
+    <HeaderNavItem href="/" text="Link 3" />
+    <HeaderNavMenu text="Menu">
+      <HeaderNavItem href="/" text="Link 1" />
+      <HeaderNavItem href="/" text="Link 2" />
+      <HeaderNavItem href="/" text="Link 3" />
+    </HeaderNavMenu>
+  </HeaderNav>
+</Header>
+
+<Content>
+  <Grid>
+    <Row>
+      <Column>
+        <slot/>
+      </Column>
+    </Row>
+  </Grid>
+</Content>
--- src/routes/track/+page.svelte
+++ src/routes/track/+page.svelte
@@ -14,12 +14,10 @@
 				? [data.logs[data.logs.length - 1].lng, data.logs[data.logs.length - 1].lat]
 				: [151.21513681183322, -33.875958176445934];
 
-  
-  let /** @type number | undefined */ minX = undefined;
-  let /** @type number | undefined */ minY = undefined;
-  let /** @type number | undefined */ maxX = undefined;
-  let /** @type number | undefined */ maxY = undefined;
-
+	let /** @type number | undefined */ minX = undefined;
+	let /** @type number | undefined */ minY = undefined;
+	let /** @type number | undefined */ maxX = undefined;
+	let /** @type number | undefined */ maxY = undefined;
 
 	onMount(() => {
 		mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
@@ -34,18 +32,18 @@
 
 		for (var i = 0; i < data.logs.length; i++) {
 			coordinates.push([data.logs[i].lng, data.logs[i].lat]);
-      if ( ! minX || minX > data.logs[i].lng ) {
-        minX = data.logs[i].lng;
-      }
-      if ( ! minY || minY > data.logs[i].lat ) {
-        minY = data.logs[i].lat;
-      }
-      if ( ! maxX || maxX < data.logs[i].lng ) {
-        maxX = data.logs[i].lng;
-      }
-      if ( ! maxY || maxY < data.logs[i].lat ) {
-        maxY = data.logs[i].lat;
-      }
+			if (!minX || minX > data.logs[i].lng) {
+				minX = data.logs[i].lng;
+			}
+			if (!minY || minY > data.logs[i].lat) {
+				minY = data.logs[i].lat;
+			}
+			if (!maxX || maxX < data.logs[i].lng) {
+				maxX = data.logs[i].lng;
+			}
+			if (!maxY || maxY < data.logs[i].lat) {
+				maxY = data.logs[i].lat;
+			}
 		}
 		const routedata = {
 			type: 'geojson',
@@ -67,16 +65,16 @@
 		data.logs.forEach((/** @type any */ log) =>
 			routedata.data.features.push({
 				type: 'Feature',
-        properties: {
-						description:
-							moment(log.timestamp_msec).format() +
-							'<br/>Coordinates: [' +
-							log.lat +
-							',' +
-							log.lng +
-							']</br>',
-            speed: log.speed
-					},
+				properties: {
+					description:
+						moment(log.timestamp_msec).format() +
+						'<br/>Coordinates: [' +
+						log.lat +
+						',' +
+						log.lng +
+						']</br>',
+					speed: log.speed
+				},
 				geometry: {
 					type: 'Point',
 					coordinates: [log.lng, log.lat]
@@ -104,12 +102,7 @@
 				type: 'circle',
 				source: 'route',
 				paint: {
-					'circle-color': [
-            'case',
-            [ '==', ['get', 'speed'], 0 ],
-            '#ff0000',
-            '#00ff00'
-          ],
+					'circle-color': ['case', ['==', ['get', 'speed'], 0], '#ff0000', '#00ff00'],
 					'circle-radius': 6
 				},
 				filter: ['==', '$type', 'Point']
@@ -121,7 +114,7 @@
 			closeOnClick: true
 		});
 
-		map.on('mouseenter', 'locations', (/** @type any */  e) => {
+		map.on('mouseenter', 'locations', (/** @type any */ e) => {
 			map.getCanvas().style.cursor = 'pointer';
 			const coordinates = e.features[0].geometry.coordinates.slice();
 			const description = e.features[0].properties.description;
@@ -141,18 +134,25 @@
 			map.getCanvas().style.cursor = '';
 			popup.remove();
 		});
-    map.fitBounds([
-      [minX, minY],
-      [maxX, maxY]
-    ], {padding: 40});
+		map.fitBounds(
+			[
+				[minX, minY],
+				[maxX, maxY]
+			],
+			{ padding: 40 }
+		);
 	});
 </script>
 
 <Grid>
 	<Row>
 		<Column>
-			<div id="map" style="min-height:500px;width:100%;margin-top:40px;" />
-			<DataTable
+			<div id="map" style="min-height:500px;width:100%;" />
+		</Column>
+	</Row>
+	<Row>
+		<Column
+			><DataTable
 				title="Travel Logs"
 				sortable
 				headers={[
@@ -171,7 +171,12 @@
 					speed: v.speed
 				}))}
 			/>
-			<Pagination bind:pageSize bind:page totalItems={data.logs.length} pageSizeInputDisabled />
-		</Column>
+			<Pagination
+				bind:pageSize
+				bind:page
+				totalItems={data.logs.length}
+				pageSizeInputDisabled
+			/></Column
+		>
 	</Row>
 </Grid>
